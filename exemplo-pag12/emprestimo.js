87d11d882c3ee4ebdfb30222ba91ab03a490cae4
Caio Ferreira <cs1919328@gmail.com>
2023-02-22 16:34:30 -0300

    att

--- exemplo-pag12/emprestimo.js
+++ exemplo-pag12/emprestimo.js
@@ -5,6 +5,9 @@ var cep = document.querySelector('#cep')
 var res1 = document.querySelector('#res1')
 var res2 = document.querySelector('#res2')
 var res3 = document.querySelector('#res3')
+var span1 = document.querySelector('#span1')
+var span2 = document.querySelector('#span2')
+var span3 = document.querySelector('#span3')
 
 var calcular = document.querySelector('#calcular')
 
@@ -21,12 +24,13 @@ calcular.addEventListener('click', function(){
     }  else if (!qtaPeriodo.value || !cep.value) { 
         alert('[ERRO] Faltam dados')
     } else {
-        var span1 = document.querySelector('#span1')
-        var span2 = document.querySelector('#span2')
-        var span3 = document.querySelector('#span3')
-
-        ///M = C.(1+i)^t
-
+        ///M = C.(1+i)^t - formula usada
+        /** calcular o emprestimo
+        * @param {number} c - valor do emprestimo
+        * @param {number} j - valor da taxa de juros
+        * @param {number} t - quantidade de período
+        * @returns {number} - valor do emprestimo
+        */
         function calcularEmprestimo(c, j, t){
             var i = j/100
             var resultado = c * ((1+i)**t)
@@ -35,11 +39,25 @@ calcular.addEventListener('click', function(){
 
         var resultadoCalculo = calcularEmprestimo(emprestimoNumber, jurosNumber, qtaPeriodoNumber)
 
+        /**
+         * Calcula o valor da parcela do empréstimo.
+         * @param {number} p - Valor total do empréstimo
+         * @param {number} qp - Quantidade de períodos
+         * @returns {number} - Valor da parcela do empréstimo
+         */
+
         function calculaParcela(p, qp) {
             return p / qp
         } 
 
         var parcela = calculaParcela(resultadoCalculo, qtaPeriodoNumber)
+
+        /**
+        * Calcula o valor total de juros do empréstimo.
+        * @param {number} j - Valor total do empréstimo
+        * @param {number} c - Valor do empréstimo
+        * @returns {number} - Valor total de juros do empréstimo
+        */
 
         function totalJuros(j, c){
             return j - c
@@ -56,6 +74,4 @@ calcular.addEventListener('click', function(){
         qtaPeriodo.value = ''
         cep.value = ''
     }
-
-
 })
